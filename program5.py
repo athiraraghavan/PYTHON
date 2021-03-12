a0d810d1f24bff3df9c7b1b9431702993d91acf3
athiraraghavan <aathira712@gmail.com>
2021-03-12 12:21:06 +0530

    commit4

---
+++ program5.py
@@ -0,0 +1,10 @@
+#co1 5.store over after 100
+n=int(input("enter the limit:"))
+list=[]
+for x in range(n):
+  x=int(input("enter the number"))
+if x<100:
+    list.append(x)
+else:
+    list.append('over')
+print(list)
