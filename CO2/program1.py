d34e9180e36d695fb1f65203068e756577b84210
athiraraghavan <aathira712@gmail.com>
2021-03-13 18:53:00 +0530

    commit co1-p1

--- CO2/program1.py
+++ CO2/program1.py
@@ -1,1 +1,5 @@
-
+fact=1
+num=int(input("enter a number:"))
+for i in range(1,num+1):
+    fact=fact * i
+print("factorial of given number is:",fact)
--- co2program1.py
+++
@@ -1,5 +0,0 @@
-fact=1
-num=int(input("enter a number:"))
-for i in range(1,num+1):
-    fact=fact * i
-print("factorial of given number is:",fact)
