77aa201161c8f016064e1a565f14712200cac846
athiraraghavan <aathira712@gmail.com>
2021-03-23 04:33:08 +0530

    commit 2

--- CO4/p2.py
+++ CO4/p2.py
@@ -1,22 +1,27 @@
-class Rectangle:
-    def __init__(self, height, width):
-        self.height = height
-        self.width = width
+class Bank:
+    def __init__(self, acno, name, type, bal):
+        self.acno = acno
+        self.name = name
+        self.type = type
+        self.bal = bal
 
-    def area(self):
-        return self.height * self.width
+    def deposite(self, amount):
+        self.bal = self.bal + amount
+        print("amount deposited successfully")
+        return self.bal
 
-    def perimeter(self):
-        return (2 * (self.height + self.width))
+    def withdraw(self, amount):
+        if amount > self.bal:
+            print("insufficient balance")
+            return self.bal
+        else:
+            self.bal = self.bal - amount
+            print("amount withdrawed successfully")
+            return self.bal
 
 
-R1 = Rectangle(4, 5)
-R2 = Rectangle(9, 2)
-print("area of rectangle1:", R1.area())
-print("perimeter of rectangle1:", R1.perimeter())
-print("area of rectangle2:", R2.area())
-print("perimeter of rectangle2:", R2.perimeter())
-if (R1.area() > R2.area()):
-    print("Area of rectangle1 is greater than area of rectangle2")
-else:
-print("Area of rectangle2 is greater than area of rectangle1")
+b = Bank(1001, "jazz", "savings", 2500)
+damount = float(input("Enter the amount to be deposited:"))
+print("Account balance:", b.deposite(damount))
+wamount = float(input("Enter the amount to be withdrawn:"))
+print("Account balance:", b.withdraw(wamount))
